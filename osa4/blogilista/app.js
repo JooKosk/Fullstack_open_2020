7d451e5e199dbcd9565e5125030e5077b39ba332
Joonas Koskela <Jomppe.koskela@gmail.com>
2020-10-15 21:34:47 +0300

    4.2 done

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,5 +1,17 @@
-const http = require('http')
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+
+module.exports = app
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,35 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
 const config = require('./utils/config')
-const cors = require('cors')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
-const blogsRouter = require('./controllers/blogs')
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-/*
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+const server = http.createServer(app)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-*/
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
