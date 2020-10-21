855139e27b3f314779aa6fa6de230d2c9a6240aa
Joonas Koskela <Jomppe.koskela@gmail.com>
2020-10-21 23:09:35 +0300

    4.12 done

--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -2,10 +2,20 @@ const mongoose = require('mongoose')
 
 
 const blogSchema = mongoose.Schema({
-  title: String,
+  title: { type: String, required: true },
   author: String,
-  url: String,
+  url: { type: String, required: true },
   likes: Number
 })
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    returnedObject.likes = returnedObject.likes === null ? 0 : returnedObject.likes
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 
 module.exports = mongoose.model('Blog', blogSchema)
--- osa4/blogilista/tests/bloglist_api.test.js
+++ osa4/blogilista/tests/bloglist_api.test.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-const blog = require('../models/blog')
 const initialBlogs = [
   {
     title: 'Go To Statement Considered Harmful',
@@ -35,6 +34,72 @@ test('blogs are returned as json', async () => {
 test ('all blogs are returned', async() => {
   const response = await api.get('/api/blogs')
 
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('blog id is defined', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0]).toBeDefined()
+})
+
+test ('http post adds valid blog', async () => {
+  const newBlog = {
+    title: 'I am a test blog',
+    author: 'Makerman man',
+    url: 'wwww.goooogol.com',
+    likes: 10,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const blogContents = response.body.map(r => r.title)
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+  expect(blogContents).toContain('I am a test blog')
+})
+
+test('new blog likes are 0 if not defined', async () => {
+  const newBlog = {
+    title: 'I am a test blog 2',
+    author: 'Makerman mans',
+    url: 'wwww.goooogol.com',
+    likes: '',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const addedBlog = response.body[response.body.length-1]
+
+  console.log(addedBlog.likes)
+  console.log(addedBlog)
+  expect(addedBlog.likes).toBe(0)
+})
+
+test('blogs cant be added without url or title', async () => {
+  const newBlog = {
+    author: 'Makerman mans',
+    likes: '',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const response = await api.get('/api/blogs')
   expect(response.body).toHaveLength(initialBlogs.length)
 })
 
