205812befe983b3eb773dd4e8928ad3d6917b174
Joonas Koskela <Jomppe.koskela@gmail.com>
2020-10-26 13:41:28 +0200

    5.1 done

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import LoginForm from './components/LoginForm'
+import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [notificationMessage, setNotificationMessage] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -14,17 +18,38 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setNotificationMessage('wrong credentials')
+      setTimeout(() => {
+        setNotificationMessage(null)
+      }, 5000)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <Notification message ={notificationMessage} />
+        <LoginForm handleLogin = {handleLogin} setUsername = {setUsername}
+        setPassword = {setPassword} password = {password} username = {username} />
+      </div>
+    )
   }
 
   return (
     <div>
-      <div>
-        <LoginForm handleLogin = {handleLogin} setUsername = {setUsername}
-        setPassword = {setPassword} password = {password} username = {username} />
-      </div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
