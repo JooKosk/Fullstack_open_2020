fe1b16fa9c2c5a2b5dd3ae93cf07722c3a3a06df
Joonas Koskela <Jomppe.koskela@gmail.com>
2020-10-26 13:16:35 +0200

    initial

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+  }
+
+  return (
+    <div>
+      <div>
+        <LoginForm handleLogin = {handleLogin} setUsername = {setUsername}
+        setPassword = {setPassword} password = {password} username = {username} />
+      </div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
